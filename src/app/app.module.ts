593fe98a9fb43eaf53a31851fad059416a604c77
rafisgarrido <rafisgarrido@gmail.com>
2023-09-29 15:25:29 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardLabelComponent } from './components/card/card-label/card-label.component';
+import { CardPricingComponent } from './components/card/card-pricing/card-pricing.component';
+import { HomeComponent } from './pages/home/home.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardLabelComponent,
+    CardPricingComponent,
+    HomeComponent,
+    MenuBarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
