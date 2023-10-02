63a5b7f149d7399f9589ddcb50f44d627c46bd70
rafisgarrido <rafisgarrido@gmail.com>
2023-10-01 21:03:42 -0300

    commit final

--- src/app/components/card/card-pricing/card-pricing.component.ts
+++ src/app/components/card/card-pricing/card-pricing.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card-pricing',
   templateUrl: './card-pricing.component.html',
   styleUrls: ['./card-pricing.component.css']
 })
-export class CardPricingComponent {
+export class CardPricingComponent implements OnInit {
 
+    @Input()
+    gamePricing:string = ""
+
+    constructor(){}
+    ngOnInit(): void {
+
+    }
 }
--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css']
 })
-export class CardComponent {
+export class CardComponent implements OnInit {
 
+  @Input()
+  gameImage:string = ""
+
+  @Input()
+  gameLabel:string = ""
+
+  @Input()
+  gamePricing:string = ""
+
+
+  constructor(){}
+  ngOnInit(): void {
+
+  }
 }
